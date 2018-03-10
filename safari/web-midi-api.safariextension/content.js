bc9516415649c1dfe2abb484a6fa646442bc9108
Sema <sema@pyva.net>
2018-03-10 15:44:26 -0500

    Safari WIP

--- safari/web-midi-api.safariextension/content.js
+++ safari/web-midi-api.safariextension/content.js
@@ -29,8 +29,9 @@ console.log('Initializing Web MIDI API...');
 
   function _newPlugin() {
     var plg = document.createElement('object');
-    plg.style.visibility='hidden';
-    plg.style.width='0px'; obj.style.height='0px';
+    plg.style.visibility = 'hidden';
+    plg.style.width = '0px';
+    plg.style.height = '0px';
     plg.type = 'audio/x-jazz';
     document.body.appendChild(plg);
     return plg.isJazz ? plg : undefined;
@@ -55,6 +56,23 @@ console.log('Initializing Web MIDI API...');
   }
   MIDIAccess.prototype.onstatechange = function() {};
 
+  function notInstalled() {
+    var div = document.createElement('div');
+    div.innerHTML = 'Web MIDI API extension requires <a href=https://jazz-soft.net style="color:red">Jazz-Plugini</a>!';
+    div.style.display = 'inline-block';
+    div.style.position = 'fixed';
+    div.style.top = '.5em';
+    div.style.left = '.5em';
+    div.style.padding = '.5em';
+    div.style.color = 'red';
+    div.style.border = '1px solid';
+    div.style.borderRadius = '.5em';
+    div.style.backgroundColor = '#ff8';
+    div.style.opacity = '.9';
+    document.body.appendChild(div);
+    setTimeout(function() { document.body.removeChild(div); }, 4000);
+  }
+
   return new Promise(function(resolve, reject) {
     if (midi_access) resolve(midi_access);
     else {
@@ -63,12 +81,12 @@ console.log('Initializing Web MIDI API...');
       document.body.appendChild(div);
       var plg = _newPlugin();
       if (plg) {
-console.log('Jazz-Plugin installed!');
         midi_access = new MIDIAccess(plg);
         resolve(midi_access);
       }
       else {
         console.log('Web MIDI extension requires Jazz-Plugin!');
+        notInstalled();
         reject('Jazz-Plugin not found!');
       }
     }
